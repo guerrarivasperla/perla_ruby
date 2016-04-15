0e65fe98485ed966f026b9f3baacb95b89d30189
Perla Guerra Rivas <guerrarivaslicet@gmail.com>
2016-04-15 08:07:01 +0800

    Primer archivo: musica

---
+++ musica.rb
@@ -0,0 +1,7 @@
+musica=["merengue","saya","pop","baladas","regueton"]
+puts"mi musica que me encanta es:"+musica[4]
+puts"========="
+musica.each{|e|
+puts"mi musica que me encanta es:"+e
+}
+gets()
